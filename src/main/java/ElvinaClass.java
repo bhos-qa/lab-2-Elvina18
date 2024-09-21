52150a2996d9b8b4933d5df34e463f382dd4782b
Elvina Aliyeva <aliyeva.elvina2004@gmail.com>
2024-09-21 22:41:37 +0400

    finalized

---
+++ src/main/java/ElvinaClass.java
@@ -0,0 +1,12 @@
+public class ElvinaClass {
+
+    public static int calculateSum(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        int result = calculateSum(10, 5);
+        System.out.println("The sum is: " + result);
+    }
+
+}
